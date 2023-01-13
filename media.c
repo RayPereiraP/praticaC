9523281dc804cb5a6c70c278e45da78f122bc332
Rayane <122495479+RayPereiraP@users.noreply.github.com>
2023-01-13 18:32:44 -0300

    Create media.c

---
+++ media.c
@@ -0,0 +1,10 @@
+int main()
+{
+    int numero1, numero2, media;
+    scanf("%d", &numero1);
+    scanf("%d", &numero2);
+    media = (numero1 + numero2)/2;
+    printf("A sua média é: %d\n", media);
+    system ("pause");
+    return (0);
+}
