cd63291a194fd784339443037a362a80ca8f2e9d
Rayane <122495479+RayPereiraP@users.noreply.github.com>
2023-01-13 18:30:47 -0300

    Update soma.c

--- soma.c
+++ soma.c
@@ -2,7 +2,7 @@ int main()
 {
     int numero1, numero2, resultado;
     scanf("%d", &numero1);
-    scanf("%d", numero2);
+    scanf("%d", &numero2);
     resultado = numero1 + numero2;
     printf("A soma dos dois números é: %d\n", resultado);
     system ("pause");
