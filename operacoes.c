d648fc948e325998ce9fbf8b19ea79dcdb9502d9
Rayane <122495479+RayPereiraP@users.noreply.github.com>
2023-01-15 19:29:43 -0300

    Create operacoes.c

---
+++ operacoes.c
@@ -0,0 +1,19 @@
+int main()
+{
+    int numero1, numero2, soma, subtracao, multiplicacao, divisao;
+    printf("Digite o primeiro número:");
+    scanf("%d", &numero1);
+    printf("Digite o segundo número:");
+    scanf("%d", &numero2);
+    soma = numero1 + numero2;
+    subtracao = numero1 - numero2;
+    multiplicacao = numero1 * numero2;
+    divisao = numero1 / numero2;
+    printf("A soma é: %d\n", soma);
+    printf("A subtracao: %d\n", subtracao);
+    print("A multiplicacao: %d\n", multiplicacao);
+    printf("A divisao: %d\n", divisao);
+    
+    system ("pause");
+    return (0);
+}
