9f70d89f379e40dcb618ce922b0f800cce114c4b
Rayane <122495479+RayPereiraP@users.noreply.github.com>
2023-01-22 20:57:13 -0300

    Create variaveis.c

---
+++ variaveis.c
@@ -0,0 +1,12 @@
+int main()
+{
+    int a = 8, b = 3;
+    if (a  > b)
+
+    {
+        printf("O núemro da variável a é maior que o número da variável b\n");
+    }
+    
+    system ("pause");
+    return (0);
+}
