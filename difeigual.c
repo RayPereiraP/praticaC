c72d746a29dd565a886fa9d5df56bbd12b306788
Rayane <122495479+RayPereiraP@users.noreply.github.com>
2023-01-22 21:13:23 -0300

    Create difeigual.c

---
+++ difeigual.c
@@ -0,0 +1,18 @@
+int main()
+{
+    printf("Digite por gentileza um número inteiro\n");
+    scanf("%d", &a);
+    printf("Digite por gentileza outro número inteiro\n");
+    scanf("%d", &b);
+
+    if (a !=b)
+    {
+        printf("a é diferente de b\n");
+    }
+    else {
+        printf("a é igual a b\n");
+    }
+    
+    system ("pause");
+    return (0);
+}
